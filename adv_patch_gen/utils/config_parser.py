9ee3e937767c91f9e3d2b2938ac8540ade73edc9
SamSamhuns <samhunsadamant@gmail.com>
2023-01-05 10:54:13 +0400

    Sep cfg parser & edict ldr

--- adv_patch_gen/utils/config_parser.py
+++ adv_patch_gen/utils/config_parser.py
@@ -6,7 +6,7 @@ import argparse
 from easydict import EasyDict as edict
 
 
-def get_config_object(cfg_path: str) -> edict:
+def load_config_object(cfg_path: str) -> edict:
     """
     Loads a config json and returns a edict object
     """
@@ -16,15 +16,13 @@ def get_config_object(cfg_path: str) -> edict:
     return edict(cfg_dict)
 
 
-def get_config_from_args() -> edict:
+def get_argparser() -> argparse.ArgumentParser:
     """
-    Get edict from chosen config file
+    Get parser with the default config argument
     """
     parser = argparse.ArgumentParser(
         description="Config file load for training adv patches")
     parser.add_argument('--cfg', '--config', type=str, dest="config", default="adv_patch_gen/configs/base.json",
                         help='Path to config file to use for adv patch generation (default: %(default)s)')
-    args = parser.parse_args()
-
-    return get_config_object(args.config)
+    return parser
 
--- train_patch.py
+++ train_patch.py
@@ -1,10 +1,7 @@
 """
 Training code for Adversarial patch training
 
-To run:
-python train_patch.py [model]
-e.g.:    python train_patch.py visdrone_v1_4cat_obj_only_tiny_gray_v1
-
+python train_patch.py --cfg config_json_file
 """
 import os
 import time
@@ -27,7 +24,7 @@ from tensorboardX import SummaryWriter
 from models.common import DetectMultiBackend
 from utils.torch_utils import select_device
 
-from adv_patch_gen.utils.config_parser import get_config_from_args
+from adv_patch_gen.utils.config_parser import get_argparser, load_config_object
 from adv_patch_gen.utils.dataset import YOLODataset
 from adv_patch_gen.utils.patch import PatchApplier, PatchTransformer
 from adv_patch_gen.utils.loss import MaxProbExtractor, SaliencyLoss, TotalVariationLoss, NPSLoss
@@ -38,28 +35,28 @@ class PatchTrainer:
     Module for training on dataset to generate adv patches
     """
 
-    def __init__(self, config: edict):
-        self.config = config
-        self.dev = config.device
+    def __init__(self, cfg: edict):
+        self.cfg = cfg
+        self.dev = cfg.device
 
-        model = DetectMultiBackend(config.weights_file, device=select_device(self.dev), dnn=False, data=None, fp16=False)
+        model = DetectMultiBackend(cfg.weights_file, device=select_device(self.dev), dnn=False, data=None, fp16=False)
         self.model = model.eval().to(self.dev)
-        self.patch_transformer = PatchTransformer(config.target_size_frac).to(self.dev)
-        self.patch_applier = PatchApplier(config.patch_alpha).to(self.dev)
-        self.prob_extractor = MaxProbExtractor(config).to(self.dev)
+        self.patch_transformer = PatchTransformer(cfg.target_size_frac).to(self.dev)
+        self.patch_applier = PatchApplier(cfg.patch_alpha).to(self.dev)
+        self.prob_extractor = MaxProbExtractor(cfg).to(self.dev)
         self.sal_loss = SaliencyLoss().to(self.dev)
-        self.nps_loss = NPSLoss(config.triplet_printfile, config.patch_size).to(self.dev)
+        self.nps_loss = NPSLoss(cfg.triplet_printfile, cfg.patch_size).to(self.dev)
         self.tv_loss = TotalVariationLoss().to(self.dev)
-        self.writer = self.init_tensorboard(config.patch_name)
+        self.writer = self.init_tensorboard(cfg.patch_name)
 
         # load dataset
         self.train_loader = torch.utils.data.DataLoader(
-            YOLODataset(config.image_dir,
-                        config.label_dir,
-                        config.max_labels,
-                        config.model_in_sz,
+            YOLODataset(cfg.image_dir,
+                        cfg.label_dir,
+                        cfg.max_labels,
+                        cfg.model_in_sz,
                         shuffle=True),
-            batch_size=self.config.batch_size,
+            batch_size=self.cfg.batch_size,
             shuffle=True,
             num_workers=10)
         self.epoch_length = len(self.train_loader)
@@ -82,21 +79,21 @@ class PatchTrainer:
         """
 
         # make output dir
-        patch_dir = os.path.join('./saved_patches', self.config.patch_name + f'_{time.strftime("%Y%m%d-%H%M%S")}')
+        patch_dir = os.path.join('./saved_patches', self.cfg.patch_name + f'_{time.strftime("%Y%m%d-%H%M%S")}')
         os.makedirs(patch_dir, exist_ok=True)
-        log_file = os.path.join(patch_dir, self.config.patch_name + '_log.txt')
-        # dump config json file
-        with open(os.path.join(patch_dir, "config.json"), 'w', encoding='utf-8') as f:
-            json.dump(self.config, f, ensure_ascii=False, indent=4)
+        log_file = os.path.join(patch_dir, self.cfg.patch_name + '_log.txt')
+        # dump cfg json file
+        with open(os.path.join(patch_dir, "cfg.json"), 'w', encoding='utf-8') as f:
+            json.dump(self.cfg, f, ensure_ascii=False, indent=4)
 
         # fix loss targets
-        loss_target = self.config.loss_target
+        loss_target = self.cfg.loss_target
         if loss_target == "obj":
-            self.config.loss_target = lambda obj, cls: obj
+            self.cfg.loss_target = lambda obj, cls: obj
         elif loss_target == "cls":
-            self.config.loss_target = lambda obj, cls: cls
+            self.cfg.loss_target = lambda obj, cls: cls
         elif loss_target == "obj * cls":
-            self.config.loss_target = lambda obj, cls: obj * cls
+            self.cfg.loss_target = lambda obj, cls: obj * cls
         else:
             raise NotImplementedError(f"Loss target {loss_target} has not been implemented")
 
@@ -122,30 +119,30 @@ class PatchTrainer:
         logging.getLogger('').addHandler(console)
         ###############################################################################
 
-        # print config values to log
+        # print cfg values to log
         logging.info("patch_dir: %s", patch_dir)
         logging.info("config_dict:")
-        config_dict = {key: value for key, value in self.config.__dict__.items(
+        config_dict = {key: value for key, value in self.cfg.__dict__.items(
         ) if not key.startswith('__') and not callable(key)}
         for k, v in config_dict.items():
             logging.info("key=%s\t val=%s", k, v)
 
         # Generate init patch
-        if self.config.patch_src == 'gray':
+        if self.cfg.patch_src == 'gray':
             adv_patch_cpu = self.generate_patch("gray")
-        elif self.config.patch_src == 'random':
+        elif self.cfg.patch_src == 'random':
             adv_patch_cpu = self.generate_patch("random")
         else:
-            adv_patch_cpu = self.read_image(self.config.patch_src)
+            adv_patch_cpu = self.read_image(self.cfg.patch_src)
         adv_patch_cpu.requires_grad = True
 
-        optimizer = optim.Adam([adv_patch_cpu], lr=self.config.start_lr, amsgrad=True)
+        optimizer = optim.Adam([adv_patch_cpu], lr=self.cfg.start_lr, amsgrad=True)
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=50)
 
         et0 = time.time()
-        for epoch in range(self.config.n_epochs):
+        for epoch in range(self.cfg.n_epochs):
             out_patch_path = os.path.join(
-                patch_dir, self.config.patch_name + '_epoch' + str(epoch) + '.jpg')
+                patch_dir, self.cfg.patch_name + '_epoch' + str(epoch) + '.jpg')
             ep_det_loss = 0
             ep_nps_loss = 0
             ep_tv_loss = 0
@@ -161,11 +158,11 @@ class PatchTrainer:
                         logging.info('TRAINING EPOCH %i, BATCH %i', epoch, i_batch)
                     adv_patch = adv_patch_cpu.to(self.dev)
                     adv_batch_t = self.patch_transformer(
-                        adv_patch, lab_batch, self.config.model_in_sz, do_rotate=True, rand_loc=False)
+                        adv_patch, lab_batch, self.cfg.model_in_sz, do_rotate=True, rand_loc=False)
                     p_img_batch = self.patch_applier(
                         img_batch, adv_batch_t)
                     p_img_batch = F.interpolate(
-                        p_img_batch, (self.config.model_in_sz[1], self.config.model_in_sz[0]))
+                        p_img_batch, (self.cfg.model_in_sz[1], self.cfg.model_in_sz[0]))
 
                     img = p_img_batch[1, :, :, ]
                     img = transforms.ToPILImage()(img.detach().cpu())
@@ -176,8 +173,8 @@ class PatchTrainer:
                     nps = self.nps_loss(adv_patch)
                     tv = self.tv_loss(adv_patch)
 
-                    nps_loss = nps * self.config.nps_mult
-                    tv_loss = tv * self.config.tv_mult
+                    nps_loss = nps * self.cfg.nps_mult
+                    tv_loss = tv * self.cfg.tv_mult
                     det_loss = torch.mean(max_prob)
                     loss = det_loss + nps_loss + \
                         torch.max(tv_loss, torch.tensor(0.1).to(self.dev))
@@ -244,7 +241,7 @@ class PatchTrainer:
         Arguments:
             patch_type: Can be 'gray' or 'random'. Whether or not generate a gray or a random patch.
         """
-        p_w, p_h = self.config.patch_size
+        p_w, p_h = self.cfg.patch_size
         if patch_type == 'gray':
             adv_patch_cpu = torch.full((3, p_h, p_w), 0.5)
         elif patch_type == 'random':
@@ -260,15 +257,17 @@ class PatchTrainer:
         :return: Returns the transformed patch as a pytorch Tensor.
         """
         patch_img = Image.open(path).convert('RGB')
-        transforms_resize = transforms.Resize(self.config.patch_size)
+        transforms_resize = transforms.Resize(self.cfg.patch_size)
         patch_img = transforms_resize(patch_img)
         adv_patch_cpu = transforms.ToTensor()(patch_img)
         return adv_patch_cpu
 
 
 def main():
-    config = get_config_from_args()
-    trainer = PatchTrainer(config)
+    parser = get_argparser()
+    args = parser.parse_args()
+    cfg = load_config_object(args.cfg)
+    trainer = PatchTrainer(cfg)
     trainer.train()
 
 
